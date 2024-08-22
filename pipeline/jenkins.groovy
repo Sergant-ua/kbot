ec0649d8a14a4aea3ec6cfd74360c17cea1bf67f
Sergant-ua <turok06@ua.fm>
2024-08-22 20:09:57 +0000

    Added steps to jenkins pipeline

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,19 +1,64 @@
 pipeline {
     agent any
-    parameters {
 
+    parameters {
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
         choice(name: 'ARCH', choices: ['amd64', 'arm64', 'all'], description: 'Pick ARCH')
-
     }
+
+    enviroment {
+        REPO = 'https://github.com/Sergant-ua/kbot'
+        BRANCH = 'develop' 
+        DOCKER_REGISTRY_URL = "https://ghcr.io"
+        GITHUB_USERNAME = "Sergant-ua"
+        
+        GITHUB_PAT = credentials('GITHUB_PAT_TOKEN')
+    }
+
     stages {
-        stage('Example') {
+        stage('clone') {
             steps {
-                echo "Build for platform ${params.OS}"
-
-                echo "Build for arch: ${params.ARCH}"
-
+                echo "Clone repository"
+                git branch: "${BRANCH}", url: "${REPO}"
             }
         }
+
+        stage('test') {
+            steps {
+                echo "Test Application"
+                sh 'make test'
+            }
+        }
+
+        stage('build') {
+            steps {
+                echo "Build Application"
+                sh 'make build'
+            }
+        }
+
+        stage('image') {
+            steps {
+                echo "Build Docker image"
+                sh 'make image'
+            }
+        }
+
+        stage('push') {
+            steps {
+                echo "Push Docker image to regisrty"
+                docker.withRegistry(DOCKER_REGISTRY_URL, "${GITHUB_USERNAME}:${GITHUB_PAT}") {
+                sh 'make push'
+            }
+        }
+
+        stage('clean') {
+            steps {
+                echo "clean"
+                sh 'make clean'
+            }
+        }
+
+
     }
 }
